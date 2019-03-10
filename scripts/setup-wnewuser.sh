2f3dd54f8ecc41227b2a1d9ceb3f21576288c633
Erin <erin@atomicbear.com>
2019-03-10 15:17:01 -0500

    more setup changes

--- scripts/setup-wnewuser.sh
+++ scripts/setup-wnewuser.sh
@@ -1,17 +1,29 @@
 #!/bin/sh
-echo "+++++ baseline update +++++"
+NEWUSER=$1
+echo "arg 1 is: $NEWUSER"
 # baseline update
 sudo apt update && sudo apt upgrade
+echo "+++++ manage user settings +++++"
+# manage user settings
+sudo groupadd -g 426942 prodteam
+sudo useradd $NEWUSER
+sudo usermod -aG prodteam ${USER}
+sudo usermod -aG prodteam $NEWUSER
 echo "+++++ install starting build repo +++++"
 # install starting build repo
 cd /
 sudo git clone https://github.com/karnthis/core-deploy.git
 sudo mv core-deploy _prod
-sudo chown -R ${USER} _prod
+sudo chown -R $NEWUSER:prodteam _prod
 echo "+++++ update acme file perms +++++"
 # update acme file perms
 cd /_prod/traefik
 sudo chmod 600 acme.json
+echo "+++++ etup ssh key for second user +++++"
+# setup ssh key for second user
+cd ~
+sudo cp .ssh ../$NEWUSER/.ssh
+sudo chown -R $NEWUSER:$NEWUSER ../$NEWUSER
 echo "+++++ nodejs install +++++"
 # nodejs install
 curl -sL https://deb.nodesource.com/setup_10.x | sudo -E bash -
@@ -28,6 +40,7 @@ sudo systemctl enable docker
 echo "+++++ update users with docker powers +++++"
 # update users with docker powers
 sudo usermod -aG docker ${USER}
+sudo usermod -aG docker $NEWUSER
 echo "+++++ install docker-compose +++++"
 # install docker-compose
 sudo apt install docker-compose
